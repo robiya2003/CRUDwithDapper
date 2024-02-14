23ea43c3c709d243bf2fe80ccadc98c68f7a2333
Muallimaqiz2003 <robiyahakimova2003@gmail.com>
2024-02-14 18:32:13 +0500

    Add project files.

---
+++ CRUDwithDapper/Controllers/UsersController.cs
@@ -0,0 +1,76 @@
+﻿using CRUDwithDapper.Models;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using System.Diagnostics.Contracts;
+
+namespace CRUDwithDapper.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public const string CONNECTINGSTRING = "Server=127.0.0.1;Port=5432;Database=forapi;User Id=postgres;Password=dfrt43i0";
+        [HttpGet]
+        public List<UserModel> GetUsers()
+        {
+            #region
+            using (NpgsqlConnection npgsqlConnection = new NpgsqlConnection(CONNECTINGSTRING))
+            {
+                return npgsqlConnection.Query<UserModel>("select * from users").ToList();
+            }
+            #endregion
+        }
+        [HttpPost]
+        public string PostUsers(UserModelTDO user)
+        {
+            #region
+            string query = $"insert into users(name,phonenumber,email,password) values" +
+                $"(@Name,@PhoneNumber,@Email,@Password)";
+            using(NpgsqlConnection npgsqlConnection = new NpgsqlConnection(CONNECTINGSTRING))
+            {
+                npgsqlConnection.Execute(query, user);
+            }
+
+            return "malumot qoshildi";
+            #endregion
+        }
+        [HttpDelete]
+        public string DeleteUsers(int id_)
+        {
+            #region
+            string query = "delete from users where id=@id";
+            using(NpgsqlConnection npgsqlConnection=new NpgsqlConnection(CONNECTINGSTRING))
+            {
+                npgsqlConnection.Execute(query,new { id=id_ });
+            }
+            return $"{id_} ochirildi";
+            #endregion
+        }
+        [HttpPut]
+        public UserModel PutUsers(int id, UserModelTDO modelTDO)
+        {
+            #region
+            string query = $"update users set " +
+                $"name=@Name," +
+                $"phonenumber=@PhoneNumber," +
+                $"email=@Email," +
+                $"password=@Password where id=@id";
+            using(NpgsqlConnection npgsqlConnection =new NpgsqlConnection(CONNECTINGSTRING))
+            {
+                UserModel user = new UserModel()
+                {
+                    Id = id,
+                    Name = modelTDO.Name,
+                    PhoneNumber = modelTDO.PhoneNumber,
+                    Email = modelTDO.Email,
+                    Password = modelTDO.Password
+                };
+                npgsqlConnection.Execute(query,user);
+                return user;
+            }
+            #endregion
+        }
+    }
+}
---
+++ CRUDwithDapper/Models/UserModelTDO.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUDwithDapper.Models
+{
+    public class UserModelTDO
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
